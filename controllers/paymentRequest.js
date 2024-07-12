73076e121a772b688f83591f0005dcd8545d7460
EmmaTheDev01 <worldoffictionrw@gmail.com>
2024-07-12 19:23:59 +0300

    Adding the controller to get approved requests

--- controllers/paymentRequest.js
+++ controllers/paymentRequest.js
@@ -1,57 +1,73 @@
-import PaymentRequest from "../models/PaymentRequest.js"
+import PaymentRequest from "../models/PaymentRequest.js";
 
-export const createPaymentRequest = async (req,res) =>{
-    const newPaymentRequest = new PaymentRequest(req.body) 
-    try{
-       const savedPaymentRequest = await newPaymentRequest.save();
-       res.status(200).json({
-        success:true,
-        message: 'PaymentRequest successful',
-        data: savedPaymentRequest, 
-       })
-
-    }catch(err){
-        res.status(500).json({
-            success:false,
-            message:'PaymentRequest failed',
-        })
-    }
+export const createPaymentRequest = async (req, res) => {
+  const newPaymentRequest = new PaymentRequest(req.body);
+  try {
+    const savedPaymentRequest = await newPaymentRequest.save();
+    res.status(200).json({
+      success: true,
+      message: 'PaymentRequest successful',
+      data: savedPaymentRequest,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: 'PaymentRequest failed',
+    });
+  }
 };
 
-//Get single PaymentRequest details
+// Get single PaymentRequest details
 
-export const getPaymentRequest = async(req, res) =>{
-    const id = req.params.id
-    try{
-        const book = await PaymentRequest.findById(id)
-        res.status(200).json({
-            success:true,
-            message:'PaymentRequest details',
-            data: book,
-        })
-    }catch(err){
-        res.status(404).json({
-            success:false,
-            message: 'No available PaymentRequest data found',
-        })
-    }
+export const getPaymentRequest = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const paymentRequest = await PaymentRequest.findById(id);
+    res.status(200).json({
+      success: true,
+      message: 'PaymentRequest details',
+      data: paymentRequest,
+    });
+  } catch (err) {
+    res.status(404).json({
+      success: false,
+      message: 'No available PaymentRequest data found',
+    });
+  }
 };
 
-//Get all boooking details
+// Get all PaymentRequest details
 
-export const getAllPaymentRequest = async(req, res) =>{
-    
-    try{
-        const allPaymentRequest = await PaymentRequest.find()
-        res.status(200).json({
-            success:true,
-            message:'PaymentRequest details',
-            data: allPaymentRequest,
-        })
-    }catch(err){
-        res.status(500).json({
-            success:false,
-            message: 'No available PaymentRequest data found',
-        })
-    }
-}
+export const getAllPaymentRequest = async (req, res) => {
+  try {
+    const allPaymentRequest = await PaymentRequest.find();
+    res.status(200).json({
+      success: true,
+      message: 'PaymentRequest details',
+      data: allPaymentRequest,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: 'No available PaymentRequest data found',
+    });
+  }
+};
+
+// Get all approved PaymentRequests
+
+export const getAllApprovedPaymentRequests = async (req, res) => {
+  try {
+    const approvedPaymentRequests = await PaymentRequest.find({ approved: true });
+    res.status(200).json({
+      success: true,
+      message: 'Approved PaymentRequests details',
+      data: approvedPaymentRequests,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: 'Failed to fetch approved PaymentRequests',
+    });
+  }
+};
--- routes/PaymentRequest.js
+++ routes/PaymentRequest.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createPaymentRequest, getAllPaymentRequest, getPaymentRequest } from "../controllers/paymentRequest.js";
+import { createPaymentRequest, getAllPaymentRequest, getPaymentRequest,getAllApprovedPaymentRequests } from "../controllers/paymentRequest.js";
 
 import { verifyAdmin, verifyUser } from "../utils/verifyToken.js";
 
@@ -7,6 +7,8 @@ const router = express.Router()
 
 router.post('/request', verifyUser, createPaymentRequest);
 router.get('/:id', verifyUser, getPaymentRequest);
-router.get('/payments', verifyAdmin, getAllPaymentRequest);
+router.get('/', verifyAdmin, getAllPaymentRequest);
+router.get('/approved',verifyAdmin, getAllApprovedPaymentRequests);
+
 
 export default router;
