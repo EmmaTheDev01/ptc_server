06d94b635e86c5f4b831e46bd993355ea805808b
EmmaTheDev01 <worldoffictionrw@gmail.com>
2024-07-13 19:48:45 +0300

    Fixing the error in getting user profile

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { login, register, getProfile } from "../controllers/authController.js";
-import { verifyToken,  } from "../utils/verifyToken.js";
+import { verifyToken, verifyUser,  } from "../utils/verifyToken.js";
 const router = express.Router()
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/profile", verifyToken, getProfile);
+router.get("/profile", verifyUser, getProfile);
 
 export default router;
