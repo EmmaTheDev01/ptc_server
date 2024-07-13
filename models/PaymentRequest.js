76109f81e137b2bee82c8ef64138a466333425a2
EmmaTheDev01 <worldoffictionrw@gmail.com>
2024-07-14 01:47:01 +0300

    Redoing the schema

--- models/PaymentRequest.js
+++ models/PaymentRequest.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const PaymentRequestSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
     required: true,
   },
   fullName: {
