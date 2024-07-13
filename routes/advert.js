9c7d99aaf00280426d4b7b65ee9002988019915f
EmmaTheDev01 <worldoffictionrw@gmail.com>
2024-07-13 17:30:15 +0300

    Fixing the get ads route

--- routes/advert.js
+++ routes/advert.js
@@ -10,18 +10,18 @@ import {
   getAdvertCounts,
   updateAdvert,
 } from "../controllers/advert.js";
-import { verifyAdmin, verifyUser } from "../utils/verifyToken.js";
-  
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
+
 const router = express.Router();
 
 // Configure multer for file uploads
 const upload = multer({ dest: "uploads/" });
 
 // Routes
-router.post("/create", createAdvert); 
-router.put("/:id", verifyAdmin, updateAdvert); 
+router.post("/create", createAdvert);
+router.put("/:id", verifyUser, updateAdvert);
 router.delete("/:id", verifyAdmin, deleteAdvert);
-router.get("/all-ads",verifyUser, findAllAdverts); // Fixed path should be defined before dynamic paths
+router.get("/all-ads", verifyToken, findAllAdverts); // Fixed path should be defined before dynamic paths
 router.get("/search/getAdvertBySearch", getAdvertBySearch); // Query parameter routes should come after fixed paths
 router.get("/search/getFeaturedAdverts", getFeaturedAdverts);
 router.get("/search/getAdvertCounts", getAdvertCounts);
